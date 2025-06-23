cb5bf784083773c8e8b663c744e56a547c2e3267
Pedro Henrique <phznff13@gmail.com>
2025-06-23 20:46:34 -0300

    add dataModel

--- StocApp/StocAppTests/StocAppTests.swift
+++ StocApp/StocAppTests/StocAppTests.swift
@@ -11,7 +11,19 @@ import Testing
 struct StocAppTests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        let equipament = Equipament.exemplo
+        print("Equipamento:")
+        print("Nome: \(equipament.nome)")                // Imprime o nome do equipamento
+        print("Tipo: \(equipament.tipo.rawValue)")       // Imprime o tipo do equipamento (convertido para string)
+        print("Quantidade: \(equipament.quantidade)")    // Imprime a quantidade do equipamento
+        
+        // Exemplo de Movimentação
+        let movimentacao = Moviment.exemplo
+        print("\nMovimentação:")
+        print("Equipamento: \(movimentacao.equipamento.nome)")    // Imprime o nome do equipamento na movimentação
+        print("Data da Retirada: \(movimentacao.data)")           // Imprime a data de retirada do equipamento
+        print("Data de Entrega: \(movimentacao.dataEntrega)")     // Imprime a data de entrega prevista
+        print("Responsável: \(movimentacao.responsavel)")          // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
 }
