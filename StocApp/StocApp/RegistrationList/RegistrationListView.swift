8fb453567068e068c5038340eb4db0ed0b6c1b64
Tamires Euzebio <pereira.tamires61@aluno.ifce.edu.br>
2025-06-23 14:54:37 -0300

    feat new empty files

---
+++ StocApp/StocApp/RegistrationList/RegistrationListView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationList.swift
+//  StocApp
+//
+//  Created by iredefbmac_41 on 23/06/25.
+//
+
+import SwiftUI
+
+struct RegistrationListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationListView()
+}
