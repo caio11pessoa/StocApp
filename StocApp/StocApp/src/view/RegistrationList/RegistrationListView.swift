e3aa7425c2579f9579d2d2470cdbe7cbd2388e58
Maria Sarah <mariasarah283@gmail.com>
2025-07-02 21:32:12 -0300

    feat: registration list view and  create component Toggle

--- StocApp/StocApp/src/view/RegistrationList/RegistrationListView.swift
+++ StocApp/StocApp/src/view/RegistrationList/RegistrationListView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct RegistrationListView: View {
+    var registers: [Moviment] = [.exemplo, .exemplo, .exemplo, .exemplo]
+    @State var isMonthlySelected: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+//            HStack(spacing: 0) {
+//                Button("Mensal") {
+//                    withAnimation {
+//                        isMonthlySelected = true
+//                    }
+//                }
+//                .foregroundColor( .black)
+//                .frame(maxWidth: .infinity)
+//                .frame(height: 38)
+//                .background(isMonthlySelected ? Color.white : Color.gray.opacity(0), in: RoundedRectangle(cornerRadius: 12))
+//                .padding(4)
+//                .shadow(radius: isMonthlySelected ? 2 : 0)
+//                
+//                Button("Anual") {
+//                    withAnimation {
+//                        isMonthlySelected = false
+//                    }
+//                }
+//                .foregroundColor( .black)
+//                .frame(maxWidth: .infinity)
+//                .frame(height: 38)
+//                .background(!isMonthlySelected ? Color.white : Color.gray.opacity(0))
+//                .padding(4)
+//                .shadow(radius: !isMonthlySelected ? 2 : 0)
+//            }
+//            .frame(height: 48)
+//            .background(Color(red: 118/225, green: 118/225, blue: 128/225, opacity: 0.12), in: RoundedRectangle(cornerRadius: 12))
+            Toggle(isMonthlySelected: $isMonthlySelected)
+            ScrollView {
+                VStack(spacing: 8) {
+                    ForEach(registers) { registro in
+                        CardHome(nomeDoEquipamento: registro.equipamento.nome, dataDevolucao: "10/10/2010", responsavel: registro.responsavel )
+                            .onTapGesture {
+                                print("clicou no item número \(String(describing: index))")
+                            }
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
 #Preview {
     RegistrationListView()
+        .padding()
 }
