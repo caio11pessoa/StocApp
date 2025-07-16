07c43d4ef679fba5b43d11d13fc4eb53284a5136
CaioPessoa <caiopessoa1298@gmail.com>
2025-07-16 19:34:24 -0300

    remove NavigationView and fix Button position

--- StocApp/StocApp/src/view/NewRegistration/NewRegistration.swift
+++ StocApp/StocApp/src/view/NewRegistration/NewRegistration.swift
@@ -15,72 +15,70 @@ struct NewRegistration: View {
     
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 24) {
+        ScrollView {
+            
+            VStack(alignment: .leading , spacing: 24) {
+                
+                
+                
+                // Objeto
+                Section {
+                    CardObject(name: "Computador")
                     
-                    // Objeto
-                    Section {
-                        CardObject(name: "Computador")
-                        
-                    } header: {
-                        Text("Objeto")
-                            .font(.title3)
-                            .bold()
-                    }
-                    
-                    // Descrição
-                    Section {
-                        Text(description)
-                            .padding(8)
-                    } header: {
-                        Text("Descrição")
-                            .font(.title3)
-                            .bold()
-                    }
-
-//
-                    
-                    // Informações
-                    Section {
-                        VStack(alignment: .leading, spacing: 16) {
-                            
-                            
-                            CustomTextField(text: $responsible, placeholder: "Responsável", icon: "person")
-                            //
-                            
-                            // Campo Data de Empréstimo
-                            CustomTextField(text: $loanDate, placeholder:" Data de Empréstimo", icon: "calendar")
-                            
-                            
-                            // Botão Adicionar Tags (agora funcional)
-                            CustomTextField(text: $tagname, placeholder: "Adicionar Tags", icon: "tag")
-                            
-                            //
-                        }
-                    } header: {
-                        Text("Informações")
-                            .font(.title3)
-                            .bold()
-                    }
-
-                    
-                    
-                    // Botão Salvar
-                    PrimaryButton(title: "Salvar", disabled: true) {
-                        print("Registro salvo:")
-                        print("Descrição: \(description)")
-                        print("Responsável: \(responsible)")
-                        print("Data: \(loanDate)")
-                    }
+                } header: {
+                    Text("Objeto")
+                        .font(.title3)
+                        .bold()
                 }
-                .padding()
+                
+                // Descrição
+                Section {
+                    Text(description)
+                        .padding(8)
+                } header: {
+                    Text("Descrição")
+                        .font(.title3)
+                        .bold()
+                }
+                
+                //
+                
+                // Informações
+                Section {
+                    VStack(alignment: .leading, spacing: 16) {
+                        
+                        
+                        CustomTextField(text: $responsible, placeholder: "Responsável", icon: "person")
+                        //
+                        
+                        // Campo Data de Empréstimo
+                        CustomTextField(text: $loanDate, placeholder:" Data de Empréstimo", icon: "calendar")
+                        
+                        
+                        // Botão Adicionar Tags (agora funcional)
+                        CustomTextField(text: $tagname, placeholder: "Adicionar Tags", icon: "tag")
+                        
+                        //
+                    }
+                } header: {
+                    Text("Informações")
+                        .font(.title3)
+                        .bold()
+                }
+                
             }
-            .navigationBarTitle("Novo registro", displayMode: .inline)
-            .navigationBarItems(leading: Button("Voltar") {
-                // Ação de voltar
-            })
         }
+        .padding()
+        
+        // Botão Salvar
+        PrimaryButton(title: "Salvar", disabled: true) {
+            print("Registro salvo:")
+            print("Descrição: \(description)")
+            print("Responsável: \(responsible)")
+            print("Data: \(loanDate)")
+        }
+        .padding()
+        .navigationBarTitle("Novo registro", displayMode: .inline)
     }
 }
 
